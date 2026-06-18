535533012d937885cc60eeb555f129b5280e3fff
Paulo Sergio Lopes <paulo.lopes44@gmail.com>
2026-06-18 16:56:56 -0300

    Ajuste das Fotos e do Javascript

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 
-document.getElementById("ano").textContent = new Date().getFullYear();
+document.addEventListener("DOMContentLoaded", function() {
+  document.getElementById("ano").textContent = new Date().getFullYear();
+});
+
+// Seleciona todos os links do menu
+const menuLinks = document.querySelectorAll('.Menu a');
+const mainContent = document.getElementById('main-content');
+
+menuLinks.forEach(link => {
+  link.addEventListener('click', (e) => {
+    e.preventDefault(); // evita o comportamento padrão
+    const newBg = link.getAttribute('data-bg'); // pega o valor do atributo data-bg
+    mainContent.style.backgroundImage = newBg; // troca o background
+  });
+});
